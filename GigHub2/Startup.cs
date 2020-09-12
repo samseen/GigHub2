81bef02c1825dc939b064c6908c0f13772c4b68a
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-12 21:25:57 +0100

    Add project files.

---
+++ GigHub2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigHub2.Startup))]
+namespace GigHub2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
