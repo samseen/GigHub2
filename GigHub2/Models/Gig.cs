ae2ceef10c3e1940a51e6799cb3487d7100932ec
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-13 23:20:48 +0100

    Added navigation properties in Gig model for ApplicationUserId and Genre.

--- GigHub2/Models/Gig.cs
+++ GigHub2/Models/Gig.cs
@@ -9,9 +9,10 @@ namespace GigHub2.Models
     public class Gig
     {
         public int Id { get; set; }
+        public ApplicationUser Artist { get; set; }
 
         [Required]
-        public ApplicationUser Artist { get; set; }
+        public string ArtistId { get; set; }
 
         public DateTime DateTime { get; set; }
 
@@ -19,7 +20,9 @@ namespace GigHub2.Models
         [StringLength(255)]
         public string Venue { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
     }
 }
