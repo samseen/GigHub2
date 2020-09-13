de7033afd1cd6f21c86075d73daa090e0112a1ed
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-13 22:39:54 +0100

    Added fields for the date and time to the view.

---
+++ GigHub2/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub2.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+    }
+}
