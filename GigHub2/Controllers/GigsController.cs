625e7a3638131df81a928b81516dd547f7815ba1
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-13 23:16:01 +0100

    Able to create a new Gig in the database.

--- GigHub2/Controllers/GigsController.cs
+++ GigHub2/Controllers/GigsController.cs
@@ -1,5 +1,6 @@
 ﻿using GigHub2.Models;
 using GigHub2.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,28 @@ namespace GigHub2.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
