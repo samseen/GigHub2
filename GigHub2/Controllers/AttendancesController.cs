cf4fddb38620548d5def47e42f7ff895fd684beb
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-19 23:08:04 +0100

    Created the Attendance web api

---
+++ GigHub2/Controllers/AttendancesController.cs
@@ -0,0 +1,31 @@
+﻿using GigHub2.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHub2.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult Attend(int gigId)
+        {
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+        }
+    }
+}
