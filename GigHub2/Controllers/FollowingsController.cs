a886fe8ae164e55a37095b76792190426c670bf2
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-20 23:46:42 +0100

    Add the following link button.

--- GigHub2/Controllers/FollowingsController.cs
+++ GigHub2/Controllers/FollowingsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GigHub2.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,30 @@ namespace GigHub2.Controllers
 {
     public class FollowingsController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == dto.FolloweeId))
+                return BadRequest("Following already exists.");
+
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
